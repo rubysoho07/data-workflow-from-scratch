5a144b8bf1df412a71d5a97f11c7c8b2ce8b58b5
Yungon Park <hahafree12@gmail.com>
2021-04-19 22:14:00 +0900

    Add script to get query result with Hooks

--- dags/external_db_dag.py
+++ dags/external_db_dag.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 
 from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
-
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 default_args = {
     'owner': 'airflow'
@@ -25,4 +26,21 @@ task_1 = PostgresOperator(
     dag=dag
 )
 
-task_1
+
+def task_test_query():
+    hook = PostgresHook(postgres_conn_id='yungon_postgres_test')
+    # hook = PostgresHook('yungon_postgres_test')
+
+    rows = hook.get_records("SELECT * FROM scheduler_core_movieschedule LIMIT 10;")
+
+    for row in rows:
+        print(row)
+
+task_2 = PythonOperator(
+    task_id='run_query_with_python',
+    python_callable=task_test_query,
+    dag=dag
+)
+
+
+task_1 >> task_2
